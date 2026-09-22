f44eae699a4ea1110d2a057dd4f5d70811466561
임석원 <rdvwg5832@gmail.com>
2026-09-22 11:17:54 +0900

    feat: Docker Hub CD 파이프라인과 실습 배포 환경 구성

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 실습용 정적 로그인 페이지는 React 마운트 지점을 사용하지 않습니다.
+const rootElement = document.getElementById('root');
+if (rootElement) {
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
